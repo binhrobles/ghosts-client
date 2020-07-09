f0808017cecddc3e1a674eff68df72d17aed1d1f
Binh Robles <binhrobles@gmail.com>
2020-07-09 16:02:39 -0700

    Feat/side by side reader (#2)
    * initial commit - map renders halfway for entryId route
    
    * Card rendering of reader
    
    * Remove unnecessary width
    
    * mock: Correctly use nested routes
    
    * mock: Add some big scrollable data
    
    * add close button + maybe (?) fix ios scrolling
    
    * clean up
    
    * Hide map behind reader on small screens
    
    * map: smoother map texture shift and flying
    
    * map: new initial center + don't trigger view when in Speak mode
    
    * Move EntryGetById up a level, so map can render entry location on direct link
    
    * cleanup
    
    * Add loading spinner on submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Grid, Page } from '@zeit-ui/react';
-import {
-  HashRouter as Router,
-  Switch,
-  Redirect,
-  Route,
-} from 'react-router-dom';
-import { About, Reader, SpeakFlow, Footer, NavBar } from './Components/index';
+import { Switch, Redirect, Route, useRouteMatch } from 'react-router-dom';
+import { About, Reader, Writer, Footer, NavBar } from './Components/index';
 import Map from './Components/common/Map';
 import Entry from './common/Entry';
 import {
   CreateClient,
   GetRecentEntries,
+  GetEntryById,
   EntriesClientContext,
 } from './Http/entries';
 import useObjectWithLocalStorage from './common/useObjectWithLocalStorage';
@@ -21,16 +17,14 @@ import config from './config';
 let entriesClient = null;
 
 function App() {
-  const [isReading, setIsReading] = React.useState(false);
   const [draftEntry, updateDraftEntry] = useObjectWithLocalStorage('entry');
-  const [loadedEntries, updateLoadedEntries] = React.useState([]);
-  const [selectedEntryId, updateSelectedEntryId] = React.useState(null);
   const namespace = 'public';
 
   if (!draftEntry) updateDraftEntry(new Entry());
   if (!entriesClient) entriesClient = CreateClient(config.baseURL);
 
   // render the most recent entries by default
+  const [loadedEntries, updateLoadedEntries] = React.useState([]);
   React.useEffect(() => {
     let isMounted = true;
     (async () => {
@@ -51,82 +45,109 @@ function App() {
     };
   }, [namespace]);
 
-  const readerCloseHandler = () => {
-    setIsReading(false);
-  };
+  // Downloads new entry when route matches
+  // have to parse `entryId` out of routeMatch, because using
+  // `routeMatch` as the UseEffect() dependency triggers way too much
+  const routeMatch = useRouteMatch(`${APP_MODES.listen.pathname}/:entryId`);
+  let entryId;
+  if (routeMatch) {
+    entryId = routeMatch.params.entryId;
+  }
+  const [isLoadingEntry, updateIsLoadingEntry] = React.useState(false);
+  const [selectedEntry, updateSelectedEntry] = React.useState(null);
+  React.useEffect(() => {
+    if (!entryId) return () => {};
 
-  const onEntryClicked = (event) => {
-    updateSelectedEntryId(event.feature.properties.entryId);
-    setIsReading(true);
-  };
+    let isMounted = true;
+    updateIsLoadingEntry(true);
+
+    // if so, download the selected entry
+    (async () => {
+      const downloaded = await GetEntryById({
+        client: entriesClient,
+        namespace,
+        id: entryId,
+      });
+
+      if (isMounted) {
+        updateSelectedEntry(downloaded);
+        updateIsLoadingEntry(false);
+      }
+    })();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [entryId, namespace]);
 
   const updateEntryLocation = ({ lng, lat }) => {
     updateDraftEntry((prev) => ({ ...prev, location: { lng, lat } }));
   };
 
   return (
-    <Router basename="/">
-      <Page size="large">
-        <NavBar />
-        <Page.Content>
-          <Switch>
-            <EntriesClientContext.Provider value={entriesClient}>
-              <Grid.Container gap={2}>
-                {/* on xs screens, map takes full width in all modes */}
-                {/* on sm screens, map takes half width in speak mode */}
-                <Route
-                  path="/(listen|speak)/"
-                  render={({ location }) => (
+    <Page size="large">
+      <NavBar />
+      <Page.Content>
+        <Switch>
+          <EntriesClientContext.Provider value={entriesClient}>
+            <Grid.Container gap={2}>
+              {/* site root is pushed to listen route */}
+              <Route exact path="/">
+                <Redirect to={APP_MODES.listen.pathname} />
+              </Route>
+
+              {/* save on map rendering by always rendering it, and adjusting render responsively */}
+              {/* on xs screens, map takes full width in speak mode, hides on click in read mode */}
+              {/* on sm screens, map takes half width in read/speak mode */}
+              <Route
+                path="/(listen|speak)/:entryId?"
+                render={({ location, match }) => {
+                  const isReading = match.params && match.params.entryId;
+                  const isSpeaking =
+                    location.pathname === APP_MODES.speak.pathname;
+                  const selectedEntryCenter = selectedEntry
+                    ? selectedEntry.location
+                    : null;
+                  return (
                     <Grid
-                      xs={24}
-                      sm={
-                        location.pathname === APP_MODES.speak.pathname ? 12 : 24
-                      }
+                      xs={isReading ? 0 : 24}
+                      sm={isReading || isSpeaking ? 12 : 24}
                     >
                       <Map
                         pathname={location.pathname}
-                        onEntryClicked={onEntryClicked}
                         layerData={loadedEntries}
                         updateEntryLocation={updateEntryLocation}
+                        selectedEntryCenter={selectedEntryCenter}
                       />
                     </Grid>
-                  )}
-                />
+                  );
+                }}
+              />
 
-                <Route exact path="/">
-                  <Redirect to={APP_MODES.listen.pathname} />
-                </Route>
-                <Route path={APP_MODES.listen.pathname}>
-                  <Grid xs={24}>
-                    <Reader
-                      isOpen={isReading}
-                      onClose={readerCloseHandler}
-                      namespace={namespace}
-                      entryId={selectedEntryId}
-                    />
-                  </Grid>
-                </Route>
-
-                {/* editor slides under map on xs screens, half width on sm and larger */}
+              {/* on xs screens, reader takes full width */}
+              {/* on sm screens, reader takes right half width */}
+              <Route path={`${APP_MODES.listen.pathname}/:entryId`}>
                 <Grid xs={24} sm={12}>
-                  <Route path={APP_MODES.speak.pathname}>
-                    <SpeakFlow
-                      entry={draftEntry}
-                      updateEntry={updateDraftEntry}
-                    />
-                  </Route>
+                  <Reader entry={selectedEntry} isLoading={isLoadingEntry} />
                 </Grid>
-              </Grid.Container>
-
-              <Route path={APP_MODES.about.pathname}>
-                <About />
               </Route>
-            </EntriesClientContext.Provider>
-          </Switch>
-        </Page.Content>
-      </Page>
+
+              {/* editor slides under map on xs screens, half width on sm and larger */}
+              <Grid xs={24} sm={12}>
+                <Route path={APP_MODES.speak.pathname}>
+                  <Writer entry={draftEntry} updateEntry={updateDraftEntry} />
+                </Route>
+              </Grid>
+            </Grid.Container>
+
+            <Route path={APP_MODES.about.pathname}>
+              <About />
+            </Route>
+          </EntriesClientContext.Provider>
+        </Switch>
+      </Page.Content>
       <Footer />
-    </Router>
+    </Page>
   );
 }
 
