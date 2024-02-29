0c8e24bf8fa30690dafd71f681ba7549ac2c982e
Binh <binhrobles@gmail.com>
2024-02-29 15:55:10 -0800

    remove dummy test

--- src/App.test.js
+++
@@ -1,5 +0,0 @@
-// dummying out test for now
-// the discussion on mapbox-gl-js-mock here:
-// https://github.com/mapbox/mapbox-gl-js-mock/issues/27
-// seems to indicate that I'll need to add some extra babel and jest config
-test('dummy test :(', () => {});
